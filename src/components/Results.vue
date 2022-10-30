f2837328ed57cd629712be2d44933d5b59e178aa
saeidjooon <saeid.esmaili2013@gmail.com>
2022-10-30 19:55:55 +0100

    30/10

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+<p>Reation Time: {{score}} ms</p>
+<p class="rank">{{rank}} </p>
+</template>
+
+<script>
+    export default{
+        props:['score'],
+        data(){
+            return{
+                rank: null
+            }
+        },
+        mounted(){
+            if(this.score < 250){
+                this.rank = "Kheyliiii khob boood"
+            }
+            else if(this.score < 400){
+                this.rank = "Bad nabooood"
+            }
+            else{
+                this.rank = "Halazone kiii bodiiii"
+            }
+        }
+    }
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: aqua;
+    font-weight: bold;
+}
+</style>
